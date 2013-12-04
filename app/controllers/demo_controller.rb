300d257cab906fc267e22245f40c94e1cfaf313f
eedonge <eedonge@gmail.com>
2013-12-04 16:06:32 +0900

    first

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,10 @@
+class DemoController < ApplicationController
+  def index
+  end
+
+  def flower
+  end
+
+  def over
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RptDemo::Application.config.secret_key_base = 'a0444adc0bc8b690d8e5375f7dd218790798efa867052881cff48c4c8ecfc5cca13d1410c663865b56d839983be08669700d040d94370a646d7c391b54396cbf'
